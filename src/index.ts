aebedb561f90c8ac9e11948d0d8a16eedc3b221b
Julio Cesar Melegari <juliocesar.melegari@gmail.com>
2022-11-21 20:41:45 -0300

    mongo connection

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,19 @@
 /* eslint-disable linebreak-style */
 import express from 'express';
+import mongoose from 'mongoose';
 
-const app = express();
 
-const port = 3001;
 
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+mongoose.connect('mongodb://localhost:27017')
+    .then(()=>{
 
-});
+        const app = express();
+        const port = 3001;
+
+        app.listen(port, () => {
+            console.log(`Server is running on http://localhost:${port} and mongo connection`);
+
+        });
+    })
+    .catch(()=> console.log('erro ao conectar ao mongoDB'));
+
